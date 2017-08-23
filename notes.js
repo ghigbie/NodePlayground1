1f5eb67be1d7c12aa9d9e5f3b9ef6095961a5b81
George Higbie <gehigbie@gmail.com>
2017-08-23 18:06:05 +0000

    Refactor code

--- notes.js
+++ notes.js
@@ -1,26 +1,32 @@
 console.log(`Starting notes.js!!`);
 const fs = require('fs');
 
+let fetchNotes = () => {
+    try{
+        let notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    }catch(e){
+        return [];
+    }
+};
+ 
+let saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));//adds notes array to the file
+};
+
 let addNote = (title, body) => {
-    let notes = []; //creates an empty array to store notes
+    let notes = fetchNotes(); //calls a function that creates an array of either existing notes or an empty array
     let note = { //defines a note
         title,
         body
     };
     
-    try{
-        let notesString = fs.readFileSync('notes-data.json'); //gets an existing array of notes and converts to string
-        notes = JSON.parse(notesString);//redefines notes array as an array with notes that aready exist if they exist    
-    }catch(e){
-        
-    }
-    
     let duplicateNotes = notes.filter((note) => note.title === title); //single line return statement 
 
     if(duplicateNotes.length === 0){
         notes.push(note); //pushes a new note to array if duplicateNotes.length has no length
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes)); //adds notes array to the file
-        console.log(`Adding note`, note.title, note.body);
+        saveNotes(notes); //adds note array to the file
+        console.log(`Adding note:`, note.title, note.body);
     }else{
         console.log(`A note with the title of: ${note.title} already exists.`);
         console.log(`Please create a new note.`);
