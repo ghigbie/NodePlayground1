6adf0e84edfd41f366a21990366322b483b95535
George Higbie <gehigbie@gmail.com>
2017-08-23 13:28:53 +0000

    Modify function

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 console.log('Starting app');
 
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFile('greeting.txt', 'Yo world!', (err) => {
+let user = os.userInfo();
+let message = `Yo ${user.username}!`;
+
+fs.appendFile('greetings.txt', message, (err) => {
     if(err){
         console.log(`Unable to write file`);
         console.log(err);
