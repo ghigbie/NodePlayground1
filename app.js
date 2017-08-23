4fa4942858082b2688ce2891d5ef419c63df8a68
George Higbie <gehigbie@gmail.com>
2017-08-23 13:48:39 +0000

    Add and export new simple function

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ let user = os.userInfo();
 let greetingMessage = `Yo ${user.username}!`;
 let ageMessage = `You are ${notes.age}.`;
 
-fs.appendFile('greetings.txt', `${greetingMessage} ${ageMessage}`, (err) => {
-    if(err){
-        console.log(`Unable to write file`);
-        console.log(err);
-    }
-});
+// fs.appendFile('greetings.txt', `${greetingMessage} ${ageMessage}`, (err) => {
+//     if(err){
+//         console.log(`Unable to write file`);
+//         console.log(err);
+//     }
+// });
+
+let res = notes.addNote();
+console.log(res);
+
+console.log(notes.addNumbers(1,7));
