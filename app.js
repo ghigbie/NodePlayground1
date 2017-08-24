55cee7f770161e76335025903a17c8211b0b3540
George Higbie <gehigbie@gmail.com>
2017-08-24 20:52:33 +0000

    Refactor code

--- app.js
+++ app.js
@@ -5,32 +5,29 @@ const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+let titleOptions = {
+                        describe: 'Title of note',
+                        demand: true,
+                        alias: 't'
+                    };
+let bodyOptions = {
+                        describe: 'Body of note',
+                        demand: true,
+                        alias: 'b'
+                   };
+                   
 const argv = yargs
     .command('add', 'Add a new note', {
-        title: {
-            describe: 'Title of note',
-            demand: true,
-            alias: 't'
-        },
-        body: {
-            describe: 'Body of note',
-            demand: true,
-            alias: 'b'
-        }
+        title: titleOptions,
+        body: bodyOptions
     })
     .command('list', 'Lists all of the notes')
-    .command('read', 'Read a specific note', {
-        title: {
-            describe: 'Title of note',
-            demand: true,
-            alias: 't'
-        }
-    }
-    )
+    .command('read', 'Read a specific note', {title: titleOptions})
+    .command('remove', 'Removes a specific note', {title: titleOptions})
     .help()
     .argv;
-
-
+    
 //let command = process.argv[2]; //using process.argv
 let command = argv._[0]; //this uses lodash to get the argv variable
 
