54e247422897064edc1451e847b314a0db5567b6
George Higbie <gehigbie@gmail.com>
2017-08-24 16:56:32 +0000

    Modify getAll function

--- notes.js
+++ notes.js
@@ -36,6 +36,7 @@ let addNote = (title, body) => {
 
 let getAll = () => {
     console.log(`Getting all notes...`);
+    return fetchNotes();
 };
 
 let getNote = (title) => {
@@ -53,6 +54,7 @@ let removeNote = (title) => {
 };
 
 let logNote = (note) => {
+    //debugger;
     console.log(`----------`);
     console.log(`Title: ${note.title}`);
     console.log(`Body: ${note.body}`);
